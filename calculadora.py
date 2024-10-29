fbe447f898165fe28d56b50bfa4eade4b10bc547
Guilherme-Parreira07 <a13651@esars.pt>
2024-10-29 09:10:54 +0000

    Função para soma

---
+++ calculadora.py
@@ -0,0 +1,13 @@
+
+#Função para soma
+def soma(x, y):
+    return x + y
+
+def calculadora():
+   num1 = float(input("Digite o primeiro número: "))
+   num2 = float(input("Digite o segundo número: "))       
+   result = soma(num1, num2)
+   print("Resultado da soma: " + str(result))
+
+# Executa a calculadora
+calculadora()
