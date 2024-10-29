ea2210ef49b2d67b6acc6b8e850a97f296c873b4
Guilherme-Parreira07 <a13651@esars.pt>
2024-10-29 09:20:39 +0000

    alteracao da funcao principal

--- calculadora.py
+++ calculadora.py
@@ -17,12 +17,33 @@ def divisão (x, y):
         return "Erro: Divisão por zero!"
     return x / y
 
-
+#Função principal
 def calculadora():
-   num1 = float(input("Digite o primeiro número: "))
-   num2 = float(input("Digite o segundo número: "))       
-   result = soma(num1, num2)
-   print("Resultado da soma: " + str(result))
+    while True:
+        operação = input ("Escolha a operação: ")
+        if operação == "subtração":
+            num1 = float(input("Digite o primeiro número: "))
+            num2 = float(input("Digite o segundo número: "))
+            result = subtração(num1, num2)
+            print("Resultado da subtração: " + str(result))
+        elif operação == "soma":
+            num1 = float(input("Digite o primeiro número: "))
+            num2 = float(input("Digite o segundo número: "))
+            result = soma(num1, num2)
+            print("Resultado da soma: " + str(result))
+        elif operação == "multiplicação":
+            num1 = float(input("Digite o primeiro número: "))
+            num2 = float(input("Digite o segundo número: "))
+            result = multiplicação(num1, num2)
+            print("Resultado da multiplicação: " + str(result))
+        elif operação == "divisão":
+            num1 = float(input("Digite o primeiro número: "))
+            num2 = float(input("Digite o segundo número: "))
+            result = divisão(num1, num2)
+            print("Resultado da divisão: " + str(result))
+        elif operação == "sair":
+            print(".....A sair da calculadora.....")
+            break
 
 # Executa a calculadora
 calculadora()
